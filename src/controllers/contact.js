738fea974746dc0307433ea33eaaad6eb385cc10
seunzone <darealseun@gmail.com>
2019-03-07 16:52:19 +0100

    delete sms of a deleted user

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,4 +1,6 @@
 import Contact from '../models/contact';
+import Sms from '../models/sms';
+import Status from '../models/status';
 /**
  * @class ContactController
  *
@@ -46,6 +48,10 @@ export default class contactController {
       const { contactId } = req.params;
       // Find and delete contact
       await Contact.findByIdAndRemove(contactId);
+      // delete the sms of the user
+      await Sms.deleteMany({ sender: contactId });
+      await Sms.deleteMany({ receiver: contactId });
+      await Status.deleteMany({ contact: contactId });
       return res.status(200).send({ message: 'Contact deleted' });
     } catch (err) {
       return res.status(400).json({ error: 'Cannot find or delete this Contact' });
