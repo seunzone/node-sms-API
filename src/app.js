0e81832e27ed0cf5b6e14c58b69404c98b1efd32
seunzone <darealseun@gmail.com>
2019-03-07 10:30:04 +0100

    connect app to mongodb database :dash:

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import express from 'express';
 import volleyball from 'volleyball';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import routes from './routes';
+
+dotenv.config();
 
 
 // Defining the Port Variable
@@ -15,6 +19,17 @@ app.use(cors());
 
 // Log requests to the console.
 app.use(volleyball);
+
+// Mongo Connection Set-Up Please this should move to the modesl folder
+const mongoDB = process.env.DATABASE_URL;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+// Get the default connection
+const db = mongoose.connection;
+
+// // Bind connection to error event (to get notification of connection errors)
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // parse request body content
 app.use(bodyParser.urlencoded({ extended: true }));
